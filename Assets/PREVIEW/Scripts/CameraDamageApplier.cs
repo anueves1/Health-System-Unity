1d09d847502bac19617b6e747c6b2aef61ae2379
anueves1 <34652784+ProfStudGit@users.noreply.github.com>
2018-10-24 02:54:48 +0200

    abc

---
+++ Assets/PREVIEW/Scripts/CameraDamageApplier.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class CameraDamageApplier : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Damage = 20;
+
+    [SerializeField]
+    private AudioClip[] m_HitClips;
+
+    [Header("Particles")]
+
+    [SerializeField]
+    private float m_ParticlesOffset = 2;
+
+    [SerializeField]
+    private GameObject m_HitParticles;
+
+    [SerializeField]
+    private float m_ParticleDestroyTime;
+
+    private AudioSource m_Source;
+
+    private void Awake()
+    {
+        //Get the source.
+        m_Source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        //Make a ray towards the position the mouse is pointing at.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            //Try to get the damageable entity component.
+            DamageableEntity entity = hit.transform.GetComponent<DamageableEntity>();
+
+            //If the object we hit has that component.
+            if (entity)
+            {
+                //If we click.
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    //Apply damage.
+                    entity.Damage(m_Damage);
+
+                    //If that last hit destroyed the object, return.
+                    if (entity.IsDead())
+                    {
+                        //Get the random index for the clip to play.
+                        var rndDestroyClip = Random.Range(0, entity.DestroyClips.Length);
+
+                        //Assign the clip.
+                        m_Source.clip = entity.DestroyClips[rndDestroyClip];
+
+                        //Play it.
+                        m_Source.Play();
+
+                        return;
+                    }
+
+                    //Get the direction from the hit point to the camera.
+                    Vector3 fromObjectToHere = transform.position - hit.point;
+
+                    //Get the hit point and add that direction to it.
+                    Vector3 instantiatePosition = hit.point;
+                    instantiatePosition += fromObjectToHere.normalized * m_ParticlesOffset;
+
+                    //Spawn some particles.
+                    GameObject hitParts = Instantiate(m_HitParticles, instantiatePosition, Quaternion.identity);
+
+                    //Destroy them after some time.
+                    Destroy(hitParts, m_ParticleDestroyTime);
+
+                    //Get the random index for the clip to play.
+                    var randomClip = Random.Range(0, m_HitClips.Length);
+
+                    //Assign the clip.
+                    m_Source.clip = m_HitClips[randomClip];
+
+                    //Play it.
+                    m_Source.Play();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PREVIEW/Scripts/DamageableEntity.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class DamageableEntity : MonoBehaviour
+{
+    public AudioClip[] DestroyClips { get { return m_DestroyClips; } }
+
+    [SerializeField]
+    private float m_MaxHealth = 100;
+
+    [Header("On Death")]
+
+    [SerializeField]
+    private AudioClip[] m_DestroyClips;
+
+    [SerializeField]
+    private GameObject m_DestroyParticles;
+
+    [SerializeField]
+    private float m_ParticleDestroyTime = 5f;
+
+    private float m_CurrentHealth;
+
+    private void Awake()
+    {
+        //Start with max health.
+        m_CurrentHealth = m_MaxHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        //Substract damage.
+        m_CurrentHealth -= damage;
+
+        //If the health is below zero, this entity is dead.
+        if (IsDead())
+            HandleDeath();
+    }
+
+    private void HandleDeath()
+    {
+        //Destroy this object.
+        Destroy(gameObject);
+
+        //Spawn destroy particles.
+        GameObject particles = Instantiate(m_DestroyParticles, transform.position, transform.rotation);
+
+        //Destroy them after some time.
+        Destroy(particles, m_ParticleDestroyTime);
+    }
+
+    public bool IsDead() { return m_CurrentHealth <= 0; }
+}
